35b6f80c7b20da4cc8ef3e7f6e6fc49d857ee4cd
kkostenko <kkostenko@assist.ru>
2020-03-16 14:48:50 +0300

    Added Pritunl installation script and user vpn config

---
+++ setupvpn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.2 main" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo rcho "deb http://repo.pritunl.com/stable/apt stretch main" | tee /etc/apt/sources.list.d/pritunl.list
+sudo apt-get install dirmngr
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv E162F504A20CDF15827F718D4B7C549A058F8B6B
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo apt-get update
+sudo apt-get --assume-yes install pritunl mongodb-server
+sudo systemctl start mongodb pritunl
+sudo systemctl enable mongodb pritunl
