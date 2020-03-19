b340a9714b1aa1aade7f0ec957d95a1d578d13f6
kkostenko <kkostenko@assist.ru>
2020-03-19 15:27:00 +0300

    Added correct mongo repo key

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xd68fa50fea312927
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt update && sudo apt install -y mongodb-org
 sudo systemctl enable --now mongod
