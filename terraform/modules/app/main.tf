6c77167bdd50886eb5e0c3582b8557edbb0e4c29
kkostenko <kkostenko@assist.ru>
2020-04-15 16:55:41 +0300

    Ansible-3

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -25,7 +25,7 @@ resource "google_compute_firewall" "firewall_puma" {
   network = "default"
   allow {
     protocol = "tcp"
-    ports = ["9292"]
+    ports = ["9292", "80"]
   }
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["reddit-app"]
